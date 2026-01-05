22f7ee81925b80d72cec26a66d56a1b787402f04
Romadhan Prasetyo <romadhanedy@gmail.com>
2026-01-05 09:33:07 +0700

    [FIX] fetch too many request issues

--- src/lib/yahoo-finance.js
+++ src/lib/yahoo-finance.js
@@ -1,8 +1,21 @@
 import YahooFinance from 'yahoo-finance2';
 
-// Singleton client to share cookies/session and suppress noisy notices.
+const customFetch = async (url, options = {}) => {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...(options.headers || {}),
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)',
+      'Accept': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Connection': 'keep-alive',
+    },
+  });
+};
+
 const yahooFinance = new YahooFinance({
   suppressNotices: ['yahooSurvey'],
+  fetch: customFetch,
 });
 
 export default yahooFinance;
